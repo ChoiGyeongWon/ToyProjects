b520dedc70c6d2470d4896ea41c03ca087d2a3f4
gyeongwon <ruddnjs1865@naver.com>
2022-08-10 21:36:30 +0900

    20220810

--- stopWatch/js/stopWatch.js
+++ stopWatch/js/stopWatch.js
@@ -1,3 +1,6 @@
+// 1 초 = 1000 밀리초
+// 60 초 (1분) = 60000 밀리초
+// 3600 초 (1시간) = 3600000 밀리초
 document.addEventListener("DOMContentLoaded", function(){
 
     const Timer = document.querySelector('.timer');
@@ -11,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function(){
     const BtnStop = ButtonWrap.querySelector('.btnStop');
     const BtnReset = ButtonWrap.querySelector('.btnReset');
 
+    let runningMiliSeconds = 0;
+    let runningSeconds = 0;
+    let runningMinutes = 0;
+    let runningHours = 0;
+
     ButtonWrap.addEventListener("click", btnClick);
 
     function btnClick(e) {
@@ -18,7 +26,10 @@ document.addEventListener("DOMContentLoaded", function(){
         let Target = e.target;
         
         if(Target.classList.contains('btnStart')) { //시작
-            setInterval(startTimer,1000);
+            setInterval(startMiliSeconds,10);
+            setInterval(startSeconds,1000);
+            setInterval(startMinutes,60000);
+            setInterval(startHours,3600000);
         }else if(Target.classList.contains('btnStop')) { //중지
             console.log('중지')
         }else if(Target.classList.contains('btnReset')) { //리셋
@@ -27,10 +38,64 @@ document.addEventListener("DOMContentLoaded", function(){
 
     }
 
-    function startTimer() {
-        console.log(11)
+
+    function startMiliSeconds() {
+
+        runningMiliSeconds += 1;
+
+        if(runningMiliSeconds < 10) {
+            Miliseconds.textContent = '0' + runningMiliSeconds;
+        }else if(runningMiliSeconds < 100) {
+            Miliseconds.textContent = runningMiliSeconds;
+        }else {
+            runningMiliSeconds = 0;
+            Miliseconds.textContent = '0' + runningMiliSeconds;
+        }
+
     }
 
+    function startSeconds() {
+
+        runningSeconds += 1;
+
+        if(runningSeconds < 10) {
+            Seconds.textContent = '0' + runningSeconds;
+        }else if(runningSeconds < 60) {
+            Seconds.textContent = runningSeconds;
+        }else {
+            runningSeconds = 0;
+            Seconds.textContent = '0' + runningSeconds;
+        }
+
+    }
+
+    function startMinutes() {
+
+        runningMinutes += 1;
+
+        if(runningMinutes < 10) {
+            Minutes.textContent = '0' + runningMinutes;
+        }else if(runningMinutes < 60) {
+            Minutes.textContent = runningMinutes;
+        }else {
+            runningMinutes = 0;
+            Minutes.textContent = '0' + runningMinutes;
+        }
+
+    }
+
+    function startHours() {
+
+        runningHours += 1;
+
+        if(runningHours < 10) {
+            Hours.textContent = '0' + runningHours;
+        }else if(runningHours < 10) {
+            Hours.textContent = runningHours;
+        }
+
+    }
+    
     function stopTimer() {
 
     }
